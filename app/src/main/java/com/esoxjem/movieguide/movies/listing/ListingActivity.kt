e3f894cf9ba2fbf34a81aa2068661be71a9e854c
Arun Sasidharan <arunsasidharan121@gmail.com>
2017-01-19 13:38:30 +0530

    Set toolbar

--- app/src/main/java/com/esoxjem/movieguide/movies/listing/ListingActivity.kt
+++ app/src/main/java/com/esoxjem/movieguide/movies/listing/ListingActivity.kt
@@ -9,7 +9,12 @@ class ListingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_listing)
+        setToolbar()
         loadListingFragment(savedInstanceState)
+    }
+
+    private fun setToolbar() {
+        supportActionBar?.title = getString(R.string.movie_guide)
     }
 
     private fun loadListingFragment(savedInstanceState: Bundle?) {
